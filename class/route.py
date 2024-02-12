df91afbae829e5c43353ed9527fba034861c965e
Diverrez morgan <97022189+morgandiverrez@users.noreply.github.com>
2024-02-12 16:05:20 +0100

    creation route

--- class/route.py
+++ class/route.py
@@ -1,31 +1,18 @@
-class Lieu:
+class Route:
+    def __init__(self, graph):
+        self.graph = graph
+        self.ordre = []
 
-  # Constructeur
-  def __init__(self, x, y, nom):
-    # Coordonnées
-    self.__x   = x
-    self.__y   = y
-    # Nom
-    self.__nom = nom
+    def generer_route_aleatoire(self):
+        self.ordre = [0]  # Début toujours au lieu 0
+        lieux_disponibles = self.graph.liste_lieux.copy()
+        lieux_disponibles.pop(0)  # Retirer le premier lieu, déjà visité
+        random.shuffle(lieux_disponibles)
+        self.ordre.extend([int(lieu.nom) for lieu in lieux_disponibles])
+        self.ordre.append(0)  # Retour au lieu de départ
 
-  # Getters
-  def getX(self):
-    return self.__x
-
-  def getY(self):
-    return self.__y
-
-  def getNom(self):
-    return self.__nom
-
-  # Méthode
-  def calcul_distance(self, destination):
-    # Récupération des coordonnées de la destination
-    x_destination = destination.getX()
-    y_destination = destination.getY()
-
-    # Calcul distance au carré x et y
-    dist_x = (x_destination - self.__x)**2
-    dist_y = (y_destination - self.__y)**2
-
-    return (dist_x + dist_y)**(1/2)
+    def calcul_distance_route(self):
+        distance_totale = 0
+        for i in range(len(self.ordre) - 1):
+            distance_totale += self.graph.matrice_od[self.ordre[i]][self.ordre[i + 1]]
+        return distance_totale
